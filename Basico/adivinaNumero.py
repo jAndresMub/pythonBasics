e4e23a12067b98fb5f1d9a5836e7f522616cda22
Andres Munoz <jandresmub@gmail.com>
2019-07-31 20:52:39 -0500

    Primer intento de programa para adivinar un numero

---
+++ Basico/adivinaNumero.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from random import randint, uniform,random
+
+def compNum(numFin):
+    numIni = int(input("Adivina el Numero, digita uno: "))
+    while numFin != numIni:
+        int(input("Intenta de nuevo digita otro numero: "))
+    
+
+def genNum():
+    return randint(1,100)
+
+
+def run():
+    print ("Vamos a comenzar, tienes que adivinar un numero del 1 al 100 ")
+    numFin = genNum()
+    compNum(numFin)
+    print("Felicitaciones acertaste!!!") 
+
+
+
+
+
+if __name__ == "__main__":
+    run ()
