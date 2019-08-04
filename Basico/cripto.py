5a9108460bcb5919c559da9363943a182ea95034
Andres Munoz <jandresmub@gmail.com>
2019-08-03 21:38:54 -0500

    se comenta el codigo

--- Basico/cripto.py
+++ Basico/cripto.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# se define el diccionario con la letra como llave y la codificacion
+# de dicha letra como el valor
 KEYS = {
     'a': 'w',
     'b': 'E',
@@ -70,6 +72,9 @@ KEYS = {
 }
 
 def cypher(message):
+    #el procedimiento es similar al de la decodificacion
+    #sino que en vez de buscar en el diccionaria cada valor
+    #se busca es cada llave y se adiciona el valor a la nueva variable
     words = message.split(' ')
     cypher_message = []
 
@@ -82,51 +87,74 @@ def cypher(message):
 
     return ' '.join(cypher_message)
 
-
+# se define la funcion para decodificar el mensaje
 def decipher(message):
+    #Crea una lista llamada words, donde cada elemento corresponde a
+    # una palabra, ka cual fue encontrada por cada espacio.
+    #split lo que hace es que separa cada vez que encuentra un espacio
+    #en este caso, si en vez de espacio se pusiera un asterisco
+    #separaria cada vez que encontrara un asterisco
+    
     words = message.split(' ')
+    
+    #se genera una lista vacia
     decipher_message = []
 
+    # Por cada palabra en la lista words se hace el bloque de codigo
     for word in words:
+        #a la lista vacia se le asigna nada
         decipher_word = ''
-
+        #por cada letra en cada palabra se ejecuta el bloque de codigo
         for letter in word:
-
+            #este for utiliza una tupla (key y value) y evalua los valores
+            # de el diccionario KEYS usando iteritems, que devuelve la 
+            #llave y el valor
+            #este for recorre el diccionario
             for key, value in KEYS.iteritems():
+                #si el valor de la letra correspondiente es igual a la letra
+                # a la lista se le suma (agrega) la llave
                 if value == letter:
                     decipher_word += key
-
+        #lo que se hizo hasta este punto fue evaluar cada letra que 
+        #corresponde a un valor del diccionario, cuando se encontrara ese valor
+        #en el diccionario a la variable decipher_word se le suma
+        #la llave, ya que se esta decodificando el mensaje
+        #en la siguiente linea lo que se hace es el append a la lista
+        #decipher_message la palabra que la se decodifico
         decipher_message.append(decipher_word)
-
+    #finalmente se devuelve la union de todas las palabras
+    #separadas por espacios
     return ' '.join(decipher_message)
 
 
 def run():
 
     while True:
-
+#imprime en la consola la interfas grafica
         command = str(raw_input('''--- * --- * --- * --- * --- * --- * --- * ---
 
-            Bienvenido a criptografía. ¿Qué deseas hacer?
+            Bienvenido a criptografï¿½a. ï¿½Quï¿½ deseas hacer?
 
             [c]ifrar mensaje
             [d]ecifrar mensaje
             [s]alir
         '''))
-
+#inicia con c para codificar
         if command == 'c':
             message = str(raw_input('Escribe tu mensaje: '))
             cypher_message = cypher(message)
             print(cypher_message)
-
+#con d decodifica
         elif command == 'd':
             message = str(raw_input('Escribe tu mensaje tu cifrado: '))
             decypher_message = decipher(message)
             print(decypher_message)
+#con s sale del programa
         elif command == 's':
             print('salir')
+#cualquier otra letra muestra un error
         else:
-            print('¡Comando no encontrado!')
+            print('ï¿½Comando no encontrado!')
 
 
 if __name__ == '__main__':
