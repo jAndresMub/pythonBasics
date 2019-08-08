96c5ee8ce6b8e011a4b784073d2d744f60f07f5e
Andres Munoz <jandresmub@gmail.com>
2019-08-07 22:06:32 -0500

    se agrega funcionalidad para poder pasar argumentos a la funcion decorador, tambien se incluye *args para poder pasar una cantidad indeterminada de argumentos a una funcion

--- Basico/decoradores.py
+++ Basico/decoradores.py
@@ -1,23 +1,26 @@
 
 
 def funcion_decoradora(funcion_parametro):
-    
-    def funcion_interior():
+    #agregando *(nombre, por convencion se pone args)
+    #se consigue que la funcion reciba un numero indeterminado de parametros
+    #como es el caso de los decoradores, la funcion, no se sabe cuantos
+    #y cuales parametros va a recibir. 
+    def funcion_interior(*args):
         
         print("vamos a hacer un calculo")
-        funcion_parametro()
+        funcion_parametro(*args)
         print("Terminamos el calculo")
 
     return funcion_interior
 
 
 @funcion_decoradora
-def suma():
-    print (15+20)
+def suma(num1, num2):
+    print (num1 + num2)
 
+@funcion_decoradora
+def resta(num1, num2):
+    print(num1-num2)
 
-def resta():
-    print(15-5)
-
-suma()
-resta()
+suma(100, 10)
+resta(15,1)
