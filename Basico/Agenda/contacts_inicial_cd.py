42053fe7f87b20e2f890dd0fd35c265b6452dd61
Andres Munoz <jandresmub@gmail.com>
2019-08-08 14:30:23 -0500

    se sube fase inicial de agenda

---
+++ Basico/Agenda/contacts_inicial_cd.py
@@ -0,0 +1,30 @@
+def run():
+
+    while True:
+        command = str(raw_input('''
+            ¿Que deseas hacer?
+            [a]ñadir contacto
+            [ac]tualizar contacto
+            [b]uscar contacto
+            [e]liminar
+            [l]istar
+            [s]alir
+        '''))
+
+        if command == 'a':
+            print('añadir contacto')
+        elif command =='ac':
+            print('actualizar contacto')
+        elif command == 'b':
+            print('buscar contacto')
+        elif command == 'e':
+            print('eliminar contacto')    
+        elif command == 'l':
+            print('listar contactos')
+        elif command == 's':
+            break
+        else:
+            print('comando no encontrado')
+
+if __name__ == "__main__":
+    run()
